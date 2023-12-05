f50b97008ba2da2f3b49698dcf6f2d5b2792d2b9
HlulaniChauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-12-05 17:45:39 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: "index.js",
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name][ext][query]',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', 
+      filename: 'index.html',   
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader', 
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: [{loader: 'html-loader',},], 
+      }
+    ],
   },
 };
